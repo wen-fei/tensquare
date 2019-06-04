a7cdcb5c095bcb959cca2d7ec981a53a766a0583
wen-fei <282699766@qq.com>
2019-06-04 21:56:12 +0800

    others

---
+++ tensquare_gathering/src/main/java/com/tensquare/gathering/GatheringApplication.java
@@ -0,0 +1,18 @@
+package com.tensquare.gathering;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+@SpringBootApplication
+public class GatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatheringApplication.class, args);
+	}
+
+	@Bean
+	public IdWorker idWorker(){
+		return new IdWorker(1, 1);
+	}
+	
+}
